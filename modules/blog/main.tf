40c090ffe8bcda602492f7ce1e9be8ce7181c3c4
RITIK MAKHIJA <ritikmakhija2002@gmail.com>
2023-03-11 08:02:49 +0530

    update

--- modules/blog/main.tf
+++ modules/blog/main.tf
@@ -14,89 +14,82 @@ data "aws_ami" "app_ami" {
   owners = [var.ami_filter.owner]
 }
 
-resource "aws_vpc" "dev" {
-  cidr_block = "10.0.0.0/16"
+
+module "blog_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = var.environment.name
+  cidr = "${var.environment.network_prefix}.0.0/16"
+
+  azs             = ["us-east-1a","us-east-1b","us-east-1c"]
+  public_subnets  = ["${var.environment.network_prefix}.101.0/24", "${var.environment.network_prefix}.102.0/24", "${var.environment.network_prefix}.103.0/24"]
+
+  enable_nat_gateway = true
+
   tags = {
-    Name = "dev-vpc"
+    Terraform = "true"
+    Environment = var.environment.name
   }
 }
 
 
-# module "blog_vpc" {
-#   source = "terraform-aws-modules/vpc/aws"
+module "blog_autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "6.5.2"
 
-#   name = var.environment.name
-#   cidr = "${var.environment.network_prefix}.0.0/16"
+  name = "${var.environment.name}-blog"
 
-#   azs             = ["us-east-1a","us-east-1b","us-east-1c"]
-#   public_subnets  = ["${var.environment.network_prefix}.101.0/24", "${var.environment.network_prefix}.102.0/24", "${var.environment.network_prefix}.103.0/24"]
+  min_size            = var.asg_min
+  max_size            = var.asg_max
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns   = module.blog_alb.target_group_arns
+  security_groups     = [module.blog_sg.security_group_id]
+  instance_type       = var.instance_type
+  image_id            = data.aws_ami.app_ami.id
+}
 
-#   enable_nat_gateway = true
+module "blog_alb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 6.0"
 
-#   tags = {
-#     Terraform = "true"
-#     Environment = var.environment.name
-#   }
-# }
+  name = "${var.environment.name}-blog-alb"
 
+  load_balancer_type = "application"
 
-# module "blog_autoscaling" {
-#   source  = "terraform-aws-modules/autoscaling/aws"
-#   version = "6.5.2"
+  vpc_id             = module.blog_vpc.vpc_id
+  subnets            = module.blog_vpc.public_subnets
+  security_groups    = [module.blog_sg.security_group_id]
 
-#   name = "${var.environment.name}-blog"
+  target_groups = [
+    {
+      name_prefix      = "${var.environment.name}-"
+      backend_protocol = "HTTP"
+      backend_port     = 80
+      target_type      = "instance"
+    }
+  ]
 
-#   min_size            = var.asg_min
-#   max_size            = var.asg_max
-#   vpc_zone_identifier = module.blog_vpc.public_subnets
-#   target_group_arns   = module.blog_alb.target_group_arns
-#   security_groups     = [module.blog_sg.security_group_id]
-#   instance_type       = var.instance_type
-#   image_id            = data.aws_ami.app_ami.id
-# }
+  http_tcp_listeners = [
+    {
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0
+    }
+  ]
 
-# module "blog_alb" {
-#   source  = "terraform-aws-modules/alb/aws"
-#   version = "~> 6.0"
+  tags = {
+    Environment = var.environment.name
+  }
+}
 
-#   name = "${var.environment.name}-blog-alb"
+module "blog_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "4.13.0"
 
-#   load_balancer_type = "application"
-
-#   vpc_id             = module.blog_vpc.vpc_id
-#   subnets            = module.blog_vpc.public_subnets
-#   security_groups    = [module.blog_sg.security_group_id]
-
-#   target_groups = [
-#     {
-#       name_prefix      = "${var.environment.name}-"
-#       backend_protocol = "HTTP"
-#       backend_port     = 80
-#       target_type      = "instance"
-#     }
-#   ]
-
-#   http_tcp_listeners = [
-#     {
-#       port               = 80
-#       protocol           = "HTTP"
-#       target_group_index = 0
-#     }
-#   ]
-
-#   tags = {
-#     Environment = var.environment.name
-#   }
-# }
-
-# module "blog_sg" {
-#   source  = "terraform-aws-modules/security-group/aws"
-#   version = "4.13.0"
-
-#   vpc_id  = module.blog_vpc.vpc_id
-#   name    = "${var.environment.name}-blog"
-#   ingress_rules = ["https-443-tcp","http-80-tcp"]
-#   ingress_cidr_blocks = ["0.0.0.0/0"]
-#   egress_rules = ["all-all"]
-#   egress_cidr_blocks = ["0.0.0.0/0"]
-# }
+  vpc_id  = module.blog_vpc.vpc_id
+  name    = "${var.environment.name}-blog"
+  ingress_rules = ["https-443-tcp","http-80-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
+}
